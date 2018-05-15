16a0057243acf55bc168844f44833703a4afa7ec
djurraa <djurraa@gmail.com>
2018-05-15 18:32:13 +0200

    All tests passed

--- Palindrom/Palindrom.Test/PalindromeTest.cs
+++ Palindrom/Palindrom.Test/PalindromeTest.cs
@@ -11,16 +11,42 @@ namespace Palindrom.Test
     public class PalindromeTest
     {
         [TestCase("Dammit I'm mad")]
-        public void Palindrome_DammitImMad_ReturnsPalindrome(string input)
+        [TestCase("Ana voli Milovana")]
+        [TestCase("Taco cat")]
+        [TestCase("11211")]
+        public void Palindrome_inputIsPalindrome_ReturnsPalindrome(string input)
         {
             // Arrange
-
             Palindrome palindrome = new Palindrome();
             String expected = "Palindrome";
-
             // Act
             String actual = palindrome.IsPalindrome(input);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
 
+        [TestCase("Dario")]
+        [TestCase("Nina Kraviz")]
+        [TestCase("Campus")]
+        public void Palindrome_input_NOTPalindrome_Returns_NotPalindrome(string input)
+        {
+            // Arrange
+            Palindrome palindrome = new Palindrome();
+            String expected = "Not palindrome";
+            // Act
+            String actual = palindrome.IsPalindrome(input);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("Campus")]
+        public void Palindrome_input_EmptyString_Returns_NotPalindrome(string input)
+        {
+            // Arrange
+            Palindrome palindrome = new Palindrome();
+            String expected = "Not palindrome";
+            // Act
+            String actual = palindrome.IsPalindrome(input);
             // Assert
             Assert.AreEqual(expected, actual);
         }
